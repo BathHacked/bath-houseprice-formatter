138957f1cd1c79e530847e23d3924c471f10c9aa
Tom <fletcher.tom@gmail.com>
2016-11-29 18:44:16 +0000

    Update to latest versions of php etc.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 sudo apt-get update;
-sudo apt-get install -yq python-dev python-pip python-setuptools build-essential curl php5-cli php5-curl git wget;
+sudo apt-get install -yq python-dev python-pip python-setuptools build-essential curl php7.0-cli php7.0-curl git wget;
 sudo pip install csvkit;
 
 php composer.phar install
