c6410e2b9c2f36dd89333085442dd4d40acd808e
Tom <fletcher.tom@gmail.com>
2016-11-29 18:44:36 +0000

    Update to latest URL for pricepaiddata

--- download.sh
+++ download.sh
@@ -2,5 +2,5 @@
 set -v
 
 cd datain;
-wget -q -N http://publicdata.landregistry.gov.uk/market-trend-data/price-paid-data/b/pp-complete.csv
+wget -q -N http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-complete.csv
 wget -q https://data.bathhacked.org/api/views/vnes-itp9/rows.csv?accessType=DOWNLOAD -O Bath___North_East_Somerset_Postcodes.csv
